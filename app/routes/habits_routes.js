2632466700a728a15c9e02ff511ba475225b71fb
steven <szapata427@yahoo.com>
2019-06-02 21:21:57 -0400

    blocks fetch because of cors to workouts datbase

--- app/routes/habits_routes.js
+++ app/routes/habits_routes.js
@@ -121,32 +121,32 @@ module.exports = function(app, db) {
 };
 
 
-module.exports = function(app, db) {
-  app.get('/setsreps', (req, res) => {
-    db.collection('setsreps').find().toArray((err, result) => {
-      res.send(result)
-    })
-  })
+// module.exports = function(app, db) {
+//   app.get('/setsreps', (req, res) => {
+//     db.collection('setsreps').find().toArray((err, result) => {
+//       res.send(result)
+//     })
+//   })
 
 
-      const collection = app.post('/setsreps', (req, res) => {
-      const setsreps = 
+//       const collection = app.post('/setsreps', (req, res) => {
+//       const setsreps = 
 
-      { 
-        setnumber: req.body.setnumber,
-        weights: req.body.weights, 
-        reps: req.body.reps, 
-        workoutid: req.body.workoutid
-      }
+//       { 
+//         setnumber: req.body.setnumber,
+//         weights: req.body.weights, 
+//         reps: req.body.reps, 
+//         workoutid: req.body.workoutid
+//       }
 
-      db.collection('setsreps').insertOne(setsreps, (err, result) => {
-        if (err) {
-          res.send({ 'error' : "An error has occurred"})
-        }
-        else {
-          res.send(result.ops[0])
-        }
-      })
-    });
+//       db.collection('setsreps').insertOne(setsreps, (err, result) => {
+//         if (err) {
+//           res.send({ 'error' : "An error has occurred"})
+//         }
+//         else {
+//           res.send(result.ops[0])
+//         }
+//       })
+//     });
 
-}
+// }
