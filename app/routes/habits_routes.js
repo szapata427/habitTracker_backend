c626b219f3dd1f0ce819b929a1d906c08387e099
steven <szapata427@yahoo.com>
2019-06-02 21:37:38 -0400

    got setsreps collections post and get request working without breaking the workouts db routes

--- app/routes/habits_routes.js
+++ app/routes/habits_routes.js
@@ -121,32 +121,3 @@ module.exports = function(app, db) {
 };
 
 
-// module.exports = function(app, db) {
-//   app.get('/setsreps', (req, res) => {
-//     db.collection('setsreps').find().toArray((err, result) => {
-//       res.send(result)
-//     })
-//   })
-
-
-//       const collection = app.post('/setsreps', (req, res) => {
-//       const setsreps = 
-
-//       { 
-//         setnumber: req.body.setnumber,
-//         weights: req.body.weights, 
-//         reps: req.body.reps, 
-//         workoutid: req.body.workoutid
-//       }
-
-//       db.collection('setsreps').insertOne(setsreps, (err, result) => {
-//         if (err) {
-//           res.send({ 'error' : "An error has occurred"})
-//         }
-//         else {
-//           res.send(result.ops[0])
-//         }
-//       })
-//     });
-
-// }
